9400a499a40ba3f584febee3e1e683c0af8a4327
Raj Aryan <raj267447@gmail.com>
2023-07-03 12:36:15 +0530

    because: fix footer and navbar

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className=" flex justify-center bottom-0 fixed  w-[100%]  p-16 pt-2 pb-2">
-    <div className='flex justify-center gap-5 w-[fit-content]  p-3 bg-black  border-gray-400  rounded-2xl '>
+    <div className='flex justify-center gap-5 w-[fit-content]  p-3 bg-[rgba(0,0,0,0.7)] border-gray-400  rounded-2xl '>
     <img className="w-12" src="./app-store.svg" alt='' />
     <img className="w-12" src="./facetime.svg" alt='' />
     <img className="w-12" src="./settings.svg" alt='' />
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <div className='h-10 p-2 pl-5 pr-5 bg-black text-white text-sm flex justify-between items-center'>
+    <div className='h-10 p-2 pl-5 pr-5 bg-[rgba(0,0,0,0.5)] text-white text-sm flex justify-between items-center'>
       <div className='h-[100%] flex gap-4 items-center'>
       {/* logo */}
       <img className="h-[80%] cursor-pointer" src="./apple.svg" alt="apple" />
